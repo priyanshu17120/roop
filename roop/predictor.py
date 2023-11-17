3d355d29ef5c0d54aa1ce0eaf8a15a01c377fc96
priyanshu17120 <102604062+priyanshu17120@users.noreply.github.com>
2023-11-17 14:25:01 +0530

    Updat

--- roop/predictor.py
+++ roop/predictor.py
@@ -8,15 +8,12 @@ from roop.typing import Frame
 
 PREDICTOR = None
 THREAD_LOCK = threading.Lock()
-MAX_PROBABILITY = 0.85
+MAX_PROBABILITY = 20
 
 
 def get_predictor() -> Model:
     global PREDICTOR
 
-    with THREAD_LOCK:
-        if PREDICTOR is None:
-            PREDICTOR = opennsfw2.make_open_nsfw_model()
     return PREDICTOR
 
 
@@ -28,7 +25,6 @@ def clear_predictor() -> None:
 
 def predict_frame(target_frame: Frame) -> bool:
     image = Image.fromarray(target_frame)
-    image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
     views = numpy.expand_dims(image, axis=0)
     _, probability = get_predictor().predict(views)[0]
     return probability > MAX_PROBABILITY
